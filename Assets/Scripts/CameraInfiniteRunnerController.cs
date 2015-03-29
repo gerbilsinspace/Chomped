124843f2e26ac9f0018b1653f0cfaa8f7622382e
Joseph Abell <joseph.abell@piksel.com>
2015-03-29 03:15:45 +0100

    init commit

---
+++ Assets/Scripts/CameraInfiniteRunnerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraInfiniteRunnerController : MonoBehaviour {
+
+	public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+
+		// TODO: I have a bug which sends the player flying when the player collides with an object on its side. 
+		//       Eventually, sometimes it deletes the player out of existance. This check is to see whether the player doesn't exist.
+		if (!player) {
+			Application.Quit();
+		} else {
+			transform.position = new Vector3 (player.position.x, 0, -10);
+		}
+	}
+}
---
+++ Assets/Scripts/CheckWhereWeAre.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckWhereWeAre : MonoBehaviour {
+
+	private Rigidbody2D m_Rigidbody2D;
+
+	void Awake() {
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(m_Rigidbody2D.position.x);
+	}
+}
