2f1bd3a87e9f361f13bcf7047f18ad9700cd7e05
Joseph Abell <joseph.abell@piksel.com>
2015-03-29 12:15:10 +0100

    Fixed Collision Bug, thanks to help from GiantBomb user @lunnington, added score count in top left, got triggers working for the pups.

---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+	float playerScore = 0;
+
+
+	public void ChangeScore(int amount) {
+		playerScore += amount;
+	}
+
+	void OnGUI() {
+		GUI.Label (new Rect(100, 000, 100, 30), "Score: " + (int)(playerScore));
+	}
+}
---
+++ Assets/Scripts/PassByPuppy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassByPuppy : MonoBehaviour {
+	HUDScript hud;
+	bool Triggered  = false;
+
+	void Awake() {
+		hud = GameObject.Find ("Main Camera").GetComponent <HUDScript> ();
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (Triggered == false&& other.tag == "Player") {
+			Triggered = true;
+			hud.ChangeScore (1);
+		}
+	}
+}
